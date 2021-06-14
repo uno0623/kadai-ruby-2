18087196e1cd3612f8e4fe441e4966524d284f95
satoshi.uno <taro.kirameki@techacademy.jp>
2021-06-14 16:06:13 +0000

    first commit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,25 @@
+#メソッドの定義
+def fizzbuzz(num)
+ 
+ if num % 3 == 0 and num % 5 == 0
+   puts 'FizzBuzz'
+   
+ elsif num % 3 == 0
+  puts 'Fizz'
+ 
+ elsif num % 5 == 0
+  puts 'Buzz'
+ 
+ else 
+   puts num
+   
+ end   
+   
+end
+
+# メソッド呼び出し
+num_max = 100
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
